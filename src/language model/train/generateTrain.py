a928a4413bf97d933fff0855eddc90fea34735ec
cynthiathena <cynthiathena00@gmail.com>
2021-12-14 00:41:21 +0700

    add data preparation n gram

---
+++ src/language model/train/generateTrain.py
@@ -0,0 +1,25 @@
+lookup = ['matikan', 'batalkan']
+set = ['pasang']
+object = ['alarm']
+at = ['pukul']
+number = ['satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh','sebelas','duabelas']
+time = ['pagi','siang','sore','malam']
+
+count_num = 0
+
+with open('train.txt', 'w') as f:
+    for lu in lookup:
+        for obj in object :
+            f.writelines('<s> ' + lu + ' ' + obj + ' </s>\n')
+    for s in set : 
+        for obj in object : 
+            for a in at : 
+                for num in number : 
+                    count_num += 1
+                    count_time = 0
+                    for t in time : 
+                        f.writelines('<s> ' + s + ' ' + obj + ' ' + a + ' ' + num+ ' ' + t + ' </s>')
+                        count_time += 1
+                        if not(count_num == len(number) and count_time == len(time)) :
+                            f.write('\n')
+                        
